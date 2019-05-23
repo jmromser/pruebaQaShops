c056f10bbc0fcd112265ee4ed1f70c2ee894096a
Pepe Romero <serrano@hotmail.com>
2019-05-23 16:06:36 +0200

    Refactorizacion Product

--- controllers/anexo1.php
+++ controllers/anexo1.php
@@ -2,26 +2,74 @@
 
 class Product
 {
-    public static function stock($productId, $quantityAvailable, $cache = false, $cacheDuration = 60, $securityStockConfig = null) {
 
-        if ($cache) {
+    public $db;
+
+    public function stock($productId, $quantityAvailable, $cache = false, $cacheDuration = 60, $securityStockConfig = null) {
+
+        $ordersQuantity = $this->ordersQuantity($this->db, $productId, $cacheDuration, $cache);
+
+        $blockedStockQuantity = $this->ordersQuantity($this->db, $productId, $cacheDuration, $cache);
+
+        $quantity = $this->quantityAvailable($quantityAvailable, $ordersQuantity, $blockedStockQuantity);
+
+        return 0;
+    }
+
+    public function ordersQuantity ($db, $productId, $cacheDuration, $cache){
+
+        if($cache){
             // Obtenemos el stock bloqueado por pedidos en curso
             $ordersQuantity = OrderLine::getDb()->cache(function ($db) use ($productId) {
-                return OrderLine::find()->select('SUM(quantity) as quantity')->joinWith('order')->where("(order.status = '" . Order::STATUS_PENDING . "' OR order.status = '" . Order::STATUS_PROCESSING . "' OR order.status = '" . Order::STATUS_WAITING_ACCEPTANCE . "') AND order_line.product_id = $productId")->scalar();
+                return OrderLine::find()->select('SUM(quantity) as quantity')
+                    ->joinWith('order')
+                    ->where("(order.status = '" . Order::STATUS_PENDING .
+                        "' OR order.status = '" . Order::STATUS_PROCESSING .
+                        "' OR order.status = '" . Order::STATUS_WAITING_ACCEPTANCE .
+                        "') AND order_line.product_id = $productId")
+                    ->scalar();
             }, $cacheDuration);
-
-            // Obtenemos el stock bloqueado
-            $blockedStockQuantity = BlockedStock::getDb()->cache(function ($db) use ($productId) {
-                return BlockedStock::find()->select('SUM(quantity) as quantity')->joinWith('shoppingCart')->where("blocked_stock.product_id = $productId AND blocked_stock_date > '" . date('Y-m-d H:i:s') . "' AND (shopping_cart_id IS NULL OR shopping_cart.status = '" . ShoppingCart::STATUS_PENDING . "')")->scalar();
-            }, $cacheDuration);
-        } else {
+        }else{
             // Obtenemos el stock bloqueado por pedidos en curso
-            $ordersQuantity = OrderLine::find()->select('SUM(quantity) as quantity')->joinWith('order')->where("(order.status = '" . Order::STATUS_PENDING . "' OR order.status = '" . Order::STATUS_PROCESSING . "' OR order.status = '" . Order::STATUS_WAITING_ACCEPTANCE . "') AND order_line.product_id = $productId")->scalar();
+            $ordersQuantity = OrderLine::find()->select('SUM(quantity) as quantity')
+                ->joinWith('order')
+                ->where("(order.status = '" . Order::STATUS_PENDING .
+                    "' OR order.status = '" . Order::STATUS_PROCESSING .
+                    "' OR order.status = '" . Order::STATUS_WAITING_ACCEPTANCE .
+                    "') AND order_line.product_id = $productId")
+                ->scalar();
 
-            // Obtenemos el stock bloqueado
-            $blockedStockQuantity = BlockedStock::find()->select('SUM(quantity) as quantity')->joinWith('shoppingCart')->where("blocked_stock.product_id = $productId AND blocked_stock_to_date > '" . date('Y-m-d H:i:s') . "' AND (shopping_cart_id IS NULL OR shopping_cart.status = '" . ShoppingCart::STATUS_PENDING . "')")->scalar();
+
         }
 
+        return $ordersQuantity;
+    }
+
+    public function blockedStockQuantity ($db, $productId, $cacheDuration, $cache){
+
+        if ($cache){
+            // Obtenemos el stock bloqueado
+            $blockedStockQuantity = BlockedStock::getDb()->cache(function ($db) use ($productId) {
+                return BlockedStock::find()->select('SUM(quantity) as quantity')
+                    ->joinWith('shoppingCart')
+                    ->where("blocked_stock.product_id = $productId AND blocked_stock_date > '" . date('Y-m-d H:i:s') .
+                        "' AND (shopping_cart_id IS NULL OR shopping_cart.status = '" . ShoppingCart::STATUS_PENDING . "')")
+                    ->scalar();
+            }, $cacheDuration);
+        }else{
+            // Obtenemos el stock bloqueado
+            $blockedStockQuantity = BlockedStock::find()->select('SUM(quantity) as quantity')
+                ->joinWith('shoppingCart')
+                ->where("blocked_stock.product_id = $productId AND blocked_stock_to_date > '" . date('Y-m-d H:i:s') .
+                    "' AND (shopping_cart_id IS NULL OR shopping_cart.status = '" . ShoppingCart::STATUS_PENDING . "')")
+                ->scalar();
+
+        }
+
+        return $blockedStockQuantity;
+    }
+
+    public function quantityAvailable ($quantityAvailable, $ordersQuantity, $blockedStockQuantity){
         // Calculamos las unidades disponibles
         if (isset($ordersQuantity) || isset($blockedStockQuantity)) {
             if ($quantityAvailable >= 0) {
@@ -50,7 +98,6 @@ class Product
             }
             return $quantityAvailable;
         }
-        return 0;
     }
 }
 
